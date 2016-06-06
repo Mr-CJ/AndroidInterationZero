0cb2d8ecaaff2ebf18d945ed9fe44ce5add277f7
Jian <jianchen@thoughtworks.com>
2016-06-06 18:22:21 +0800

    add MainActivity

---
+++ app/src/main/java/com/jian/zero/interation/androidinterationzero/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jian.zero.interation.androidinterationzero;
+
+import com.jian.zero.interation.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+}
